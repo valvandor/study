f820c459dd1fce0c9467f7da781645e701dd9ed9
Sincov Vitalii <sincov@algont.ru>
2022-10-05 18:15:35 +0300

    feat: complete json task

---
+++ usefull_stuff/file_formats/json/const.py
@@ -0,0 +1,15 @@
+"""
+Module with constants
+"""
+ENCODING = 'utf-8'
+
+ORDERS = 'orders'
+
+INDENTS = '4'
+FILE_FORMAT = '.json'
+
+ITEM = 'item'
+QUANTITY = 'quantity'
+PRICE = 'price'
+BUYER = 'buyer'
+DATE = 'date'
