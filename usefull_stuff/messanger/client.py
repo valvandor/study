b9f6910d9bf42fe617f5e2848ab850dad76b9113
Sincov Vitalii <sincov@algont.ru>
2022-10-09 12:05:14 +0300

    refactor: create ClientSocket class

--- usefull_stuff/messanger/client.py
+++ usefull_stuff/messanger/client.py
@@ -5,59 +5,84 @@ Client part of messanger
 import json
 import socket
 import time
+from _socket import SocketType
+
 from common import const
 from common.utils import get_message, send_message
 from config.config import bind_config_file
+from exceptions import IncompleteConfigError
 
 
-def create_presence(account_name: str = 'Guest') -> dict:
+class ClientSocket:
     """
-    Generates a client presence request
+    Class represents client socket logic
     """
-    out = {
-        const.ACTION: const.PRESENCE,
-        const.TIME: time.time(),
-        const.USER: {
-            const.ACCOUNT_NAME: account_name
+    def __init__(self):
+        self._config = self._get_config()
+
+    @staticmethod
+    def _get_config() -> dict:
+        config_file = bind_config_file()
+        with open(config_file) as f:
+            return json.load(f)
+
+    @property
+    def _connected_client_socket(self) -> SocketType:
+        """
+        Initializes a socket via config values
+
+        Returns:
+            connected socket
+        """
+        try:
+            server_port = self._config['listen_port']
+            server_address = self._config['listen_address']
+        except KeyError as err:
+            raise IncompleteConfigError from err
+
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_socket.connect((server_address, server_port))
+        return tcp_socket
+
+    def send_presence_message(self):
+        """
+        Sends presence message
+        """
+        client_socket = self._connected_client_socket
+        message_to_server = self._create_presence()
+        send_message(client_socket, message_to_server, self._config)
+        try:
+            answer = self._process_ans(get_message(client_socket, self._config))
+            print(answer)
+        except (ValueError, json.JSONDecodeError):
+            print('Failed to decode server message.')
+
+    @staticmethod
+    def _create_presence(account_name: str = 'Guest') -> dict:
+        """
+        Generates a client presence request
+        """
+        out = {
+            const.ACTION: const.PRESENCE,
+            const.TIME: time.time(),
+            const.USER: {
+                const.ACCOUNT_NAME: account_name
+            }
         }
-    }
-    return out
+        return out
 
-
-def process_ans(message):
-    """
-    Parses the server response
-    """
-    if const.RESPONSE in message:
-        if message[const.RESPONSE] == 200:
-            return '200 : OK'
-        return f'400 : {message[const.ERROR]}'
-    raise ValueError
-
-
-def main():
-    """
-    Loads command line options
-    """
-    config_file = bind_config_file()
-    with open(config_file) as f:
-        config = json.load(f)
-
-    server_port = config['listen_port']
-    server_address = config['listen_address']
-
-    # Socket initialization and exchange
-
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.connect((server_address, server_port))
-    message_to_server = create_presence()
-    send_message(transport, message_to_server, config)
-    try:
-        answer = process_ans(get_message(transport, config))
-        print(answer)
-    except (ValueError, json.JSONDecodeError):
-        print('Failed to decode server message.')
+    @staticmethod
+    def _process_ans(message):
+        """
+        Parses the server response
+        """
+        if const.RESPONSE in message:
+            if message[const.RESPONSE] == 200:
+                return '200 : OK'
+            return f'400 : {message[const.ERROR]}'
+        raise ValueError
 
 
 if __name__ == '__main__':
-    main()
+    transport = ClientSocket()
+    transport.send_presence_message()
