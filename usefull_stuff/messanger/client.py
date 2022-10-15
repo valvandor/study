7f437e02ed6eb42c02fbb254c806947272de88ee
Sincov Vitalii <sincov@algont.ru>
2022-10-15 20:56:49 +0300

    chore: add log messages in a code

--- usefull_stuff/messanger/client.py
+++ usefull_stuff/messanger/client.py
@@ -3,6 +3,7 @@ Client part of messanger
 """
 
 import json
+import logging
 import socket
 from _socket import SocketType
 from datetime import timezone, datetime
@@ -12,6 +13,7 @@ from common import const
 from common.config_mixin import ConfigMixin
 from common.exceptions import IncompleteConfigError
 from common.abstract_socket import AbstractSocket
+logger = logging.getLogger('client')
 
 
 class ClientSocket(ConfigMixin, AbstractSocket):
@@ -34,10 +36,12 @@ class ClientSocket(ConfigMixin, AbstractSocket):
             server_port = self._config['listen_port']
             server_address = self._config['listen_address']
         except KeyError as err:
+            logger.exception("Unable to get options from config")
             raise IncompleteConfigError from err
 
         tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         tcp_socket.connect((server_address, server_port))
+        logger.info("Create client socket [server address %s:%s]", server_address, server_port)
         self._client_socket = tcp_socket
 
     def send_presence_message(self):
@@ -50,9 +54,9 @@ class ClientSocket(ConfigMixin, AbstractSocket):
         self.send_message(client_socket, message_to_server)
         try:
             answer = self._process_ans(self.get_message(client_socket))
-            print(answer)
+            logger.debug("Got answer from a server [%s]", answer)
         except (ValueError, json.JSONDecodeError):
-            print('Failed to decode server message.')
+            logger.exception("Failed to decode server message")
 
     @staticmethod
     def _create_presence(account_name: str = 'Guest') -> dict:
@@ -66,6 +70,7 @@ class ClientSocket(ConfigMixin, AbstractSocket):
                 const.ACCOUNT_NAME: account_name
             }
         }
+        logger.debug("Create presence message from [%s] account", account_name)
         return out
 
     @staticmethod
@@ -82,5 +87,6 @@ class ClientSocket(ConfigMixin, AbstractSocket):
 
 if __name__ == '__main__':
     client_transport = ClientSocket()
+    client_transport.setup_log_config(in_project_dir=True)
     client_transport.create_connected_client_socket()
     client_transport.send_presence_message()
