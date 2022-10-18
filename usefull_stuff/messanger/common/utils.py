089c6fc1ebed116496d9c3544b4712b660401415
Sincov Vitalii <sincov@algont.ru>
2022-10-18 20:36:59 +0300

    feat: create decorator for deep logging
    (cherry picked from commit 2f4390dcd29ba745859bbc7fe72004d5e16375ac)

--- usefull_stuff/messanger/common/utils.py
+++ usefull_stuff/messanger/common/utils.py
@@ -3,6 +3,8 @@ Helpfully utils
 """
 import logging
 import sys
+from datetime import datetime
+from functools import wraps
 
 
 def get_common_logger() -> logging.Logger:
@@ -14,3 +16,30 @@ def get_common_logger() -> logging.Logger:
     """
     logger_name = 'server' if 'server.py' in sys.argv[0] else 'client'
     return logging.getLogger(logger_name)
+
+
+def logged(logger):
+    """
+    Decorator for wrapping methods with logging
+
+    Args:
+        logger: logger to use
+
+    Returns:
+        decorated function
+    """
+    def wrap_method(func_to_log):
+
+        @wraps(func_to_log)
+        def wrapped(*args, **kwargs):
+            start_time = datetime.now()
+            func_name = func_to_log.__name__
+            logger.debug(f'Enter {func_name} method (args={args}, kwargs={kwargs})')
+
+            result = func_to_log(*args, **kwargs)
+
+            spent_time = round((datetime.now() - start_time).total_seconds() * 1000)
+            logger.debug(f'Result {func_name} return: {result}, {spent_time} ms')
+            return result
+        return wrapped
+    return wrap_method
