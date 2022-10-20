a8b408c2d16447f4b1e7cf0774261d8919207c43
Sincov Vitalii <sincov@algont.ru>
2022-10-20 22:04:13 +0300

    feat: implement broadcast messages from the server

--- usefull_stuff/messanger/common/const.py
+++ usefull_stuff/messanger/common/const.py
@@ -3,10 +3,17 @@ Constants
 """
 
 # keys for JIM protocol
+
 ACTION = 'action'
+
+# possible types of messages
+MESSAGE = 'message'
+
 TIME = 'time'
 USER = 'user'
 ACCOUNT_NAME = 'account_name'
+MESSAGE_TEXT = 'message_text'
+SENDER = 'sender'
 
 PRESENCE = 'presence'
 RESPONSE = 'response'
